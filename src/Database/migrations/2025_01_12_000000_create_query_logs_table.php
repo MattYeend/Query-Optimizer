09b87dbb56716c42c8d57a3713d4855a6194993b
Matt Yeend <mattyeend1@hotmail.co.uk>
2025-01-12 17:14:14 +0000

    #28 Update to migration

--- src/Database/migrations/2025_01_12_000000_create_query_logs_table.php
+++ src/Database/migrations/2025_01_12_000000_create_query_logs_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+class CreateQueryLogsTable extends Migration
+{
     public function up()
     {
         Schema::create('query_logs', function (Blueprint $table) {
@@ -20,4 +21,4 @@ return new class extends Migration {
     {
         Schema::dropIfExists('query_logs');
     }
-};
+}
